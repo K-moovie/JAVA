d27f33e8b67fe52972fe6631c60552ff1a0f364b
KIM <kyha97@naver.com>
2019-09-20 00:36:42 +0900

    Add Ch02.game2

--- java_practice/src/ch01/beer/ModifiedBeerSong.java
+++ java_practice/src/ch01/beer/ModifiedBeerSong.java
@@ -1,14 +1,11 @@
 package ch01.beer;
 
-public class BeerSong {
+public class ModifiedBeerSong {
     public static void main(String[] args){
         int beerNum = 5;
         String word = "bottles"; //복수형
         
         while(beerNum > 0){ //while문 조건식 
-            if(beerNum == 1){ //beerNum이 1인 조건식
-                word = "bottle"; //단수형
-            }
             
             System.out.println(beerNum + " " + word + " of beer on the wall"); 
             //beerNum의 상태를 출력
@@ -17,7 +14,9 @@ public class BeerSong {
             System.out.println("Take one down.");
             System.out.println("Pass it around.");
             beerNum = beerNum-1; //int형 변수 beerNum을 99부터 1씩 뺀다
-            
+            if(beerNum == 1){ //beerNum이 1인 조건식
+                word = "bottle"; //단수형
+            }
             if(beerNum > 0){
                 System.out.println(beerNum + " " + word + " of beer on the wall"); 
                 //beerNum의 상태를 출력
