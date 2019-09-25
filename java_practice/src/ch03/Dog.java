8a0e1c4daa2d1c8257ca88776ef14264f5a3ce1b
kyha97 <kyha97@naver.com>
2019-09-25 20:26:52 +0900

    Revise ch03

--- java_practice/src/ch03/Dog.java
+++ java_practice/src/ch03/Dog.java
@@ -4,15 +4,15 @@ package ch03;
 public class Dog {
     private String name;
 
-    public Dog(String name) {
+    public Dog(String name) { // constructor
         this.name = name;
     }
 
-    public String getName() {
+    public String getName() { // getter
         return name;
     }
     
-    public void bark(){
+    public void bark(){ // 사용자 정의 함수
         System.out.println(name + "이(가) 왈!하고 짖습니다.");
     }
     
--- java_practice/src/ch03/DogArrayTest.java
+++ java_practice/src/ch03/DogArrayTest.java
@@ -5,14 +5,16 @@ public class DogArrayTest {
     public void test(){
         Dog[] myDogs = new Dog[3];
         
-        myDogs[0] = new Dog("Fred");
+        myDogs[0] = new Dog("Fred"); 
         myDogs[1] = new Dog("Marge");
         myDogs[2] = new Dog("Bart");
-        
+        // Dog는 Primitive type이 아닌 사용자 정의형이므로
+        // 객체배열을 생성하고 각 배열마다 new를 사용해 주어야 한다.
+        // (배열크기 + 1)만큼 new사용
         System.out.print("마지막 개의 이름 : ");
         System.out.println(myDogs[myDogs.length-1].getName());
         System.out.println("------------");
-        
+         
         int x = 0;
         while(x < myDogs.length){
             myDogs[x].bark();
@@ -20,5 +22,7 @@ public class DogArrayTest {
         }
         System.out.println("------------");
         java.util.Arrays.stream(myDogs).forEach(d->d.bark());
+        // java.util.Arrays로 myDogs를 stream형태로 만들어 준다.
+        // stream의 forEach메소드로 각 객체배열의 bark()메소드를 호출한다.
     }
 }
